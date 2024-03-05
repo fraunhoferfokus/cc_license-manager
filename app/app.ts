d2491a9df01f5dde507922a4fd628f868fc91699
tng <the-anh.nguyen@fokus.fraunhofer.de>
2024-03-05 06:25:31 +0100

    Adding variable

--- app/app.ts
+++ app/app.ts
@@ -107,7 +107,7 @@ APP.use(
         store: redisStore,
         resave: false, // required: force lightweight session keep alive (touch)
         saveUninitialized: false, // recommended: only save session when data exists
-        secret: "keyboard cat",
+        secret: process.env.SESSION_SECRET! || 'keyboard cat',
     })
 )
 
